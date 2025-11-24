81f486f9818de584494da35c79fa8ff406ed9467
nguyen minh anh <nanh2832003@gmail.com>
2025-11-24 08:57:29 +0700

    Update file bat

--- TaskTracker/task-cli.bat
+++ TaskTracker/task-cli.bat
@@ -1,4 +1,4 @@
 @echo off
-javac -d out src\Main.java src\CommandParser.java src\TaskRepository.java src\TaskService.java
+javac -d out src\Main.java src\CommandParser.java src\TaskRepository.java src\TaskService.java src\Task.java src\JsonUtils.java
 
 java -cp out Main %*
